ce952bf12d0d6bac7c00b9bd90cc757c3023de4a
jyeung <jakeyeung@gmail.com>
2014-05-23 10:57:07 -0700

    Init commit: adjust pvalues by BH method

---
+++ mirna_analysis/adjust_pvalues.R
@@ -0,0 +1,32 @@
+# Jake Yeung
+# 23 Aug 2013
+# modified May 22 2014 to a more geneirc function
+
+# ARgs --------------------------------------------------------------------
+
+
+args <- commandArgs(trailingOnly=TRUE)
+filename <- args[1]
+output_filename <- args[2]
+
+
+# Main --------------------------------------------------------------------
+
+pval_summary <- read.table(filename, 
+                           header=TRUE,
+						   row.names=1,
+						   sep='\t')
+
+pvals <- pval_summary$pval    # Second column.
+
+pvals.adjusted <- p.adjust(pvals, method='BH')
+
+# Create new dataframe with pvals.adjusted as new column
+# place new column to right of unadjusted pvals.
+output_df <- cbind(pval_summary, bh_adj_pval=pvals.adjusted)
+
+# Save df to a new filename
+write.table(output_df, file=output_filename, sep='\t', 
+			row.names=TRUE, quote=FALSE, col.names=NA)
+print(paste('Adjusted', length(pvals.adjusted), 'p-values.'))
+print(paste('BH-adjusted file saved to', output_filename))
