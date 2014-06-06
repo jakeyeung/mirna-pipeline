2d6f30d93e1dfd800a155c44f83fd7f4c654cb9e
jyeung <jakeyeung@gmail.com>
2014-06-05 17:07:56 -0700

    Write output with no rownames specified

--- mirna_analysis/adjust_pvalues.R
+++ mirna_analysis/adjust_pvalues.R
@@ -26,6 +26,6 @@ output_df <- cbind(pval_summary, bh_adj_pval=pvals.adjusted)
 
 # Save df to a new filename
 write.table(output_df, file=output_filename, sep='\t', 
-			row.names=TRUE, quote=FALSE, col.names=NA)
+			row.names=FALSE, quote=FALSE)
 print(paste('Adjusted', length(pvals.adjusted), 'p-values.'))
 print(paste('BH-adjusted file saved to', output_filename))
