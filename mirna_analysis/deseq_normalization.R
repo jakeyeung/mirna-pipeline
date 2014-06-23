002950cf9b863ca0ab0274d201b58112de20e1c7
jyeung <jakeyeung@gmail.com>
2014-06-23 15:58:04 -0700

    Provide option to ignore first few columns. Purpose is to reduce reshaping work to do deseq

--- mirna_analysis/deseq_normalization.R
+++ mirna_analysis/deseq_normalization.R
@@ -11,21 +11,29 @@ args <- commandArgs(trailingOnly=TRUE)
 
 inputfile <- args[1]
 outputfile <- args[2]
+ignore_first_n_cols <- args[3]
+ignore_first_n_cols <- as.numeric(ignore_first_n_cols)
 
-countTable_All <- read.table(file=inputfile, head=TRUE, row.names=1, sep="\t")
+countTable_All <- read.table(file=inputfile, head=TRUE, sep="\t")
 
-n_samples <- ncol(countTable_All)    # one column is microRNA id
+# Get subset containing only counts data
+countTable_All.subset <- countTable_All[, -(1:ignore_first_n_cols)]
+
+n_samples <- ncol(countTable_All.subset)    # one column is microRNA id
 print(n_samples)
 conds <- factor(rep("NA", n_samples))
 
-str(countTable_All)
+str(countTable_All.subset)
 
-cds_All <- newCountDataSet(countTable_All, conds)
+cds_All <- newCountDataSet(countTable_All.subset, conds)
 
 cds_All <- estimateSizeFactors(cds_All)
 cds_All <- estimateDispersions(cds_All)
 
 normalizedCounts_All <- t(t(counts(cds_All)) / sizeFactors(cds_All))
 
-write.table(normalizedCounts_All, file=outputfile, sep="\t", quote=FALSE, col.names=NA)
+# Append normalizedCounts_All back to countTable_All
+countTable_All <- cbind(countTable_All[, (1:ignore_first_n_cols)], normalizedCounts_All)
+
+write.table(countTable_All, file=outputfile, sep="\t", quote=FALSE, row.names=FALSE)
 
