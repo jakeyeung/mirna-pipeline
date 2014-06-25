eb6c84a74dd40ec683a6bebc4b422d78d8976e57
jyeung <jakeyeung@gmail.com>
2014-06-24 17:43:30 -0700

    Print statements to keep track of progress

--- mirna_analysis/get_target_scores.R
+++ mirna_analysis/get_target_scores.R
@@ -10,6 +10,9 @@ args <- commandArgs(trailingOnly=TRUE)
 mirna_name <- args[1]
 outfile <- args[2]
 
+print(paste("Trying to extract miRNA: ", mirna_name))
 mirna_targetscores <- targetScoreMatrix[[which(names(targetScoreMatrix) == mirna_name)]]
 
 write.table(mirna_targetscores, outfile, sep='\t', row.names=TRUE, col.names=NA, quote=FALSE)
+print(paste(mirna_name, " target genes written to file: ", outfile))
+
