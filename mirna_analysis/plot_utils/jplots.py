940e5b68020c2e8a3f1475703a1ac001d5151a1a
jyeung <jakeyeung@gmail.com>
2014-06-26 15:30:37 -0700

    Draw horizontal line at median.

--- mirna_analysis/plot_utils/jplots.py
+++ mirna_analysis/plot_utils/jplots.py
@@ -7,6 +7,7 @@ Functions for plotting
 
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def jitter_values(lst, jitter=0.1):
@@ -23,7 +24,8 @@ def plot_vertical_scatter(y_values,
                           jitter=True,
                           title='Plot title',
                           xlabel='x-label',
-                          ylabel='y-label'):
+                          ylabel='y-label',
+                          draw_median=True):
     '''
     Takes as input a list of y-values to be scatterplotted.
     Includes jitter to show values
@@ -31,17 +33,26 @@ def plot_vertical_scatter(y_values,
     Plots a vertical scatter. So they will all have the same x value
     '''
     x_values = [0] * len(y_values)
+
+
     if jitter:
         x_values = jitter_values(x_values)
     fig = plt.figure()
     ax = plt.axes()
     plt.scatter(x_values, y_values)
     plt.xlim([-1, 1])
+    plt.ylim([-5, 5])
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     # Turn off x axis
     ax.axes.get_xaxis().set_ticks([])
+    # Draw horiznotal line at median
+    if draw_median:
+        median_y = np.median(y_values)
+        xmin = -0.2
+        xmax = 0.2
+        plt.hlines(median_y, xmin, xmax)
     plt.show()
 
 
