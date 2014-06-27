a124390a7f58113fe1950e2108fe93a9ef3da1b6
jyeung <jakeyeung@gmail.com>
2014-06-27 14:51:43 -0700

    Extended function to plot n genes

--- mirna_analysis/plot_utils/jplots.py
+++ mirna_analysis/plot_utils/jplots.py
@@ -20,6 +20,45 @@ def jitter_values(lst, jitter=0.1):
     return lst
 
 
+def is_list_of_lists(input_list):
+    '''
+    Check if list contains list inside
+    '''
+    return any(isinstance(el, list) for el in input_list)
+
+
+def frange(x, y, jump):
+    '''
+    Create range, but jumps are floats.
+    '''
+    if jump == 0:
+        print 'Warning: incrementing by %s results in infinite loop.' % jump
+        sys.exit()
+    while x <= y:
+        yield x
+        x += jump
+
+
+def get_x_values_for_scatter(y_values, xlim_min, xlim_max):
+    '''
+    We want scatterplot to plot arbitrary number of vectors
+    of y-values. The x-values should be assigned such that they
+    are evenly spaced amongst the plot.
+    '''
+    x_values = []
+
+    n_vectors = len(y_values)
+
+    size_yvals = len(y_values[0])
+
+    increment = float((xlim_max - xlim_min)) / (n_vectors + 1.)
+
+    for xval in frange(xlim_min + increment, xlim_max - increment, increment):
+        x_values.append([xval] * size_yvals)
+
+    return x_values
+
+
 def plot_vertical_scatter(y_values,
                           jitter=True,
                           title='Plot title',
@@ -28,31 +67,47 @@ def plot_vertical_scatter(y_values,
                           draw_median=True):
     '''
     Takes as input a list of y-values to be scatterplotted.
+
+    Can be a list of lists or just a single list
+
     Includes jitter to show values
 
     Plots a vertical scatter. So they will all have the same x value
     '''
-    x_values = [0] * len(y_values)
+    xlim_min = -1
+    xlim_max = 1
+    hline_radius = 0.2    # half of horizontal line to be drawn
 
+    # check if list of lists, if it is not, make it a list of list
+    if not is_list_of_lists(y_values):
+        y_values = [y_values]
 
-    if jitter:
-        x_values = jitter_values(x_values)
+    # Create x-values as list of lists based on number of vectors
+    # supplied.
+    x_values = get_x_values_for_scatter(y_values, xlim_min, xlim_max)
+
     fig = plt.figure()
     ax = plt.axes()
-    plt.scatter(x_values, y_values)
-    plt.xlim([-1, 1])
+
+    for x, y in zip(x_values, y_values):
+        # Draw horiznotal line at median
+        if draw_median:
+            median_y = np.median(y)
+            xmin = x[0] - hline_radius
+            xmax = x[0] + hline_radius
+            plt.hlines(median_y, xmin, xmax)
+        # Plot x-y values
+        if jitter:
+            x = jitter_values(x)
+        plt.scatter(x, y)
+    # Plot settings
+    plt.xlim([xlim_min, xlim_max])
     plt.ylim([-7, 7])
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     # Turn off x axis
     ax.axes.get_xaxis().set_ticks([])
-    # Draw horiznotal line at median
-    if draw_median:
-        median_y = np.median(y_values)
-        xmin = -0.2
-        xmax = 0.2
-        plt.hlines(median_y, xmin, xmax)
     plt.show()
 
 
